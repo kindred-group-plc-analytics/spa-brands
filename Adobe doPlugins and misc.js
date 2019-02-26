40769f3e5cd9736f0e11b375839e80f20facc415
Konstantinos Papadopoulos <konpap@lon-fvfxq1prhv2h.unibet.com>
2019-02-26 10:57:19 +0000

    Moved Previous page variables back into the view event.

--- Adobe doPlugins and misc.js
+++ Adobe doPlugins and misc.js
@@ -83,16 +83,15 @@ if (a == 'view') {
 
 b.full_page_url = window.location.href;
 
-b.adb_previous_page_name = b['cp.utag_main__prevpage'];
-b.adb_previous_full_url = b['cp.utag_main__prevnew_full_url'];
-
 // Code that needs to execute only on page views and not on link events.
 if (a == 'view') {
     // Over-write cookies only on view events.    
     utag.loader.SC('utag_main', {
         '_prevpage': b.adb_pageName + ';exp-session'
     });
+    b.adb_previous_page_name = b['cp.utag_main__prevpage'];
     utag.loader.SC('utag_main', {
         '_prevnew_full_url': b.full_page_url + ';exp-session'
     });
+    b.adb_previous_full_url = b['cp.utag_main__prevnew_full_url'];
 }
